6df2710e7427d9d115f5bc46e32c6628c452019d
HuirongZhang <1219912825@qq.com>
2026-01-06 19:18:37 +0800

    变更

--- jeepay-business/src/main/java/com/jeequan/jeepay/business/controller/HomeServiceController.java
+++ jeepay-business/src/main/java/com/jeequan/jeepay/business/controller/HomeServiceController.java
@@ -1,5 +1,8 @@
 package com.jeequan.jeepay.business.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/home-service")
 public class HomeServiceController {
